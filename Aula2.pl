35b6307ab556907dd276a568e76a0fdac32b269f
Kbzinho-66 <gvalles@ucs.br>
2022-03-23 19:54:12 -0300

    Algumas regras não tavam bem certas

--- Aula2.pl
+++ Aula2.pl
@@ -2,18 +2,17 @@
 % Existem alguns registos antigos que indicam que o casal José e Maria criou dois filhos, o João e a Ana. 
 % Que a Ana teve duas filhas, a Helena e a Joana, também parece ser verdade, segundo os
 % mesmos registos. Além disso, o Mário é filho do João, pois muito se orgulha disso. 
-% Estranho também, foi constatar que o Carlos nasceu da relaçãoo entre a Helena e o Mário.
+% Estranho também, foi constatar que o Carlos nasceu da relação entre a Helena e o Mário.
 
-pais(X,Y).
-pais(jose, joao).
-pais(jose, ana).
-pais(maria, joao).
-pais(maria, ana).
-pais(ana, helena).
-pais(ana, joana).
-pais(joao, mario).
-pais(helena, carlos).
-pais(mario, carlos).
+progenitor(jose, joao).
+progenitor(jose, ana).
+progenitor(maria, joao).
+progenitor(maria, ana).
+progenitor(ana, helena).
+progenitor(ana, joana).
+progenitor(joao, mario).
+progenitor(helena, carlos).
+progenitor(mario, carlos).
 
 sexo(jose, masc).
 sexo(joao, masc).
@@ -24,61 +23,60 @@ sexo(ana, fem).
 sexo(helena, fem).
 sexo(joana, fem).
 
-irmao(X,Y) :-
-    X \= Y,
-    pais(Z,X),
-    pais(Z,Y),
-    sexo(X, masc).
-
-irma(X,Y) :-
-    X \= Y,
-    pais(Z,X),
-    pais(Z,Y),
-    sexo(X, fem).
-
-descendente(E1,E2) :-
-    pais(E2,E1).
-descendente(E1,E2) :-
-    pais(E2,E1),
-    descendente(E2,E3).
+% X é descendente de Y se 
+    % Y for um dos progenitores de X.
+descendente(X,Y) :-
+    progenitor(Y,X).
+% X é descendente de Y se
+    % existe um progenitor Z de X e 
+    % Z é descendente de Y.
+descendente(X,Y) :-
+    progenitor(Z,X),
+    descendente(Z,Y).
 
 pai(X,Y) :-
-    pais(X,Y),
+    progenitor(X,Y),
     sexo(X, masc).
 
 mae(X,Y) :-
-    pais(X,Y),
+    progenitor(X,Y),
     sexo(X, fem).
 
-avoo(X,Y) :-
-    sexo(X, masc),
-    pais(X,Z),
-    pais(Z,Y).
-
-avoh(X,Y) :-
-    sexo(X, fem),
-    pais(X,Z),
-    pais(Z,Y).
-
 irmaos(X,Y) :-
-    X \= Y,
-    pais(Z,X),
-    pais(Z,Y).
+    progenitor(Z,X),
+    progenitor(Z,Y),
+    X \== Y.
 
+irmao(X,Y) :-
+    irmaos(X,Y),
+    sexo(X, masc).
+
+irma(X,Y) :-
+    irmaos(X,Y),
+    sexo(X, fem).
+
+% X é tio de Y se 
+    % X for homem e
+    % X for um dos irmaos de Z e
+    % Z é um dos progenitores de Y.
 tio(X,Y) :-
     sexo(X, masc),
     irmaos(X,Z),
-    pais(Z,Y).
+    progenitor(Z,Y).
 
+% X é tia de Y se 
+    % X for mulher e
+    % X for um dos irmaos de Z e
+    % Z é um dos progenitores de Y.
 tia(X,Y) :-
     sexo(X, fem),
     irmaos(X,Z),
-    pais(Z,Y).
+    progenitor(Z,Y).
 
 primos(X,Y) :-
     X \= Y,
-    pais(Px,X),
-    pais(Py,Y),
+    progenitor(Px,X),
+    progenitor(Py,Y),
     irmaos(Px, Py).
 
 primo(X,Y) :-
@@ -89,10 +87,24 @@ prima(X,Y) :-
     primos(X,Y),
     sexo(X, fem).
 
+avoo(X,Y) :-
+    sexo(X, masc),
+    progenitor(X,Z),
+    progenitor(Z,Y).
+
+avoh(X,Y) :-
+    sexo(X, fem),
+    progenitor(X,Z),
+    progenitor(Z,Y).
+
+
 % 1) João é filho de José?
 
-% ?- pais(jose, joao).
+% ?- progenitor(jose, joao).
 
 % 2) Quem são os filhos da Maria?
 
-pais()
+% mae(maria, F).
+
+% 3) Quem são os primos do Mário?
+
